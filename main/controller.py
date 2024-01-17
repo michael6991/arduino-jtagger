0bce4a529e9509ff340ec1007612cd89d9a1d30f
michael6991 <michael.vigdorchik96@gmail.com>
2024-01-17 09:51:47 +0200

    Update controller.py

--- main/controller.py
+++ main/controller.py
@@ -7,95 +7,101 @@
 @author Michael Vigdorchik
 """
 
-
 import serial
-from sys import stdout
+import sys
+import struct
+import time
 
 
 # globals
 INPUT_CHAR = ">"
 
 # uart propreties
-PORT = "COM4"
+PORT = "COM4"  # or "/dev/ttyUSB0"
 BAUD = 115200
 TIMEOUT = 0.5  # sec
 
 
-s = serial.Serial(port=PORT, baudrate=BAUD, timeout=TIMEOUT)
+if len(sys.argv) < 3:
+    print("Usage:")
+    print("    python3 controller.py [COM4 for windows | /dev/ttyUSB0 for unix] 115200")
+    sys.exit(0)
 
 
-def writer(data: str):
-    """
-    Attempt to write data to serial device.
-    @param data: String data to write.
-    """
-    if data == "":
-        # user entered not data.
-        # happens when just pressing "ENTER" key, to pass input function
-        data = " "  # instead insert a SPACE character (0x20)
-    s.write(bytes(data, "utf-8"))
-    s.flush()
-
-
-def reader():
-    """
-    Try to read lines from serial device, till the INPUT_CHAR
-    character is received.
-    @return True if the INPUT_CHAR character is received, else
-    return None if nothing was received. ("")
-    """
-    recv = "0"
-
-    while recv != "":
-        try:
-            recv = s.readline()  # read a '\n' terminated line or timeout
-            recv = recv.decode("cp437")  # decodes utf-8 and more
-            stdout.write(recv)
-            stdout.flush()
-
-            if INPUT_CHAR in recv:  # user input is required
-                return True
-        
-        except serial.SerialTimeoutException:
-            print("Reader SerialTimeoutException")
-            break
-    return None
+class Communicator():
+    def __init__(self) -> None:
+        self.s = serial.Serial(port=sys.argv[1],
+                               baudrate=sys.argv[2],
+                               timeout=TIMEOUT,
+                               parity=serial.PARITY_NONE,
+                               stopbits=serial.STOPBITS_ONE,
+                               bytesize=serial.EIGHTBITS)
+        self.s.flushInput()
+        self.s.flushOutput()
     
+    def writer(self, data: str):
+        """
+        Attempt to write data to serial device.
+        @param data: String data to write.
+        """
+        if data == "":
+            # user entered not data.
+            # happens when just pressing "ENTER" key, to pass input function
+            data = " "  # instead insert a SPACE character (0x20)
+        self.s.write(bytes(data, "utf-8"))
+        self.s.flush()
 
+    def reader(self):
+        """
+        Attempt to read lines from serial device, till the INPUT_CHAR
+        character is received.
+        @return True if the INPUT_CHAR character is received, else
+        return None if nothing was received. ("")
+        """
+        r = "0"
+        while r != "":
+            try:
+                r = self.s.readline()  # read a '\n' terminated line or timeout
+                r = r.decode("cp437")  # decodes utf-8 and more
+                sys.stdout.write(r)
+                sys.stdout.flush()
+                self.s.flushInput()
+                if INPUT_CHAR in r:  # user input is required
+                    return True
+            except serial.SerialTimeoutException as err:
+                pass
+        return None
 
-def init_arduino():
-    """
-    Read arduino "start message" and send a start signal.
-    """
-    # wait for arduino to send start message
-    while reader() is None:
-        pass
-    # wait for user to send a start char to Arduino
-    while input() != "s":
-        pass
-    writer("s")
+    def init_arduino(self):
+        """
+        Read arduino "start message" and send a start signal.
+        """
+        # wait for arduino to send start message
+        while self.reader() is None:
+            pass
+        # wait for user to send a start char to Arduino
+        while input() != "s":
+            pass
+        self.writer("s")
 
 
 def main():
-    s.reset_input_buffer()
-    s.reset_output_buffer()
-    s.flushInput()
-    s.flushOutput()
-
-    # Starting Arduino
-    init_arduino()
+    c = Communicator()
+    c.init_arduino()
     
     # Entering Arduino's main menu
     while True:
-        r = reader()
+        r = c.reader()
         if r is True:
             command = input()
-            writer(command)
+        if command == "exit":
+            break
+        else:
+            c.writer(command)
+            c.reader()
+    c.close()
 
-        if command == 'z':
-            reader()
-            break    
-    s.close()
 
 if __name__ == "__main__":
+    print(f"running {__name__}")
     main()
